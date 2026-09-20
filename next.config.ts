be87f5cf8e54c863256681567458896ca07b7bf2
R1007 <dheerajkoila97@gmail.com>
2026-09-20 18:20:55 +0530

    added * i used it for connecting network to the mobile

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
 
-  allowedDevOrigins: ['10.52.161.117'],
+  allowedDevOrigins: ['*'],
 };
 
 export default nextConfig;
